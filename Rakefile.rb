a7adb42ffb0e378f006f6835265eec7688c8a4a9
Bob "Wombat" Hogg <wombat@rwhogg.site>
2021-01-01 09:45:09 -0500

    Installer icons

--- Rakefile.rb
+++ Rakefile.rb
@@ -3,7 +3,7 @@ task default: %W[bundle]
 task bundle: %W[dist/controller-list.exe dist/controller-list-installer.exe]
 
 %W[controller-list.py].each do |pyfile|
-    sh "pyinstaller.exe --noconsole --onefile #{pyfile}"
+    sh "pyinstaller.exe --noconsole --onefile --icon icon.ico #{pyfile}"
 end
 
 %W[installer.nsi].each do |installerscript|
--- installer.nsi
+++ installer.nsi
@@ -1,6 +1,7 @@
 !define VERSION "1.0.0"
 Name "Controller Lister ${VERSION}"
 OutFile "dist\controller-list-installer.exe"
+Icon "icon.ico"
 
 InstallDir "$PROGRAMFILES64\Controller Lister"
 
