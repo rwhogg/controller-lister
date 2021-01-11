3c5ee927ed34df14d71a6fa26bc71b7aa39ba670
Bob "Wombat" Hogg <wombat@rwhogg.site>
2021-01-10 22:43:49 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="controller-lister",
+    version="1.0.0",
+    description="Controller lister",
+    license="LGPL-2.1",
+    long_description=long_description,
+    author="Bob Wombat Hogg",
+    author_email="wombat@rwhogg.site",
+    url="https://github.com/rwhogg/controller-lister",
+    packages=["controller-lister"],
+    install_requires=["guizero", "pygame"],
+)
